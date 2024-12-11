10fa0a30aafbb006c1408ae87cdec67f6f924d79
Damian Makki <damian@makki.pro>
2024-12-11 09:35:48 -0600

    add `react-helmet`

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import HerbMap from "../components/Map";
+import { Helmet } from "react-helmet";
 
 export default function Home() {
   return (
-    <HerbMap />
+    <>
+      <Helmet>
+        <title>Little Herb Places | Home</title>
+      </Helmet>
+      <HerbMap />
+    </>
   )
 }
