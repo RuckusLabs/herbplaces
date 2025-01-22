380fb4658149efd8e290e4c33a95999d01bc4c63
Damian Makki <damian@makki.pro>
2025-01-22 13:21:10 -0700

    shop page scaffolding

---
+++ src/pages/Shop/Shop.jsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet";
+
+export default function Shop() {
+  return (
+    <>
+      <Helmet>
+        <title>Little Herb Places | Shop</title>
+      </Helmet>
+      <h1>Shop</h1>
+    </>
+  )
+}
