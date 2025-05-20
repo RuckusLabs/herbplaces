cc4647f7af9ae5ebe51f5236849221892d1d293e
Damian Makki <damian@makki.pro>
2025-05-20 15:00:18 -0700

    add `svgr` to build process

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr' 
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,12 @@ export default defineConfig({
     },
   },
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        // svgr options
+      },
+    }),
+  ],
 })
