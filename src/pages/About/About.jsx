8dded293259bbc94c5c308ab752d80469bee07ad
Damian Makki <damian@makki.pro>
2025-05-20 15:05:43 -0700

    adjust about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,16 +5,15 @@ export default function About() {
 
   return (
     <>
-    <Helmet>
-      <title>Little Herb Places | About</title>
-    </Helmet>
-    <div className={styles.container}>
-      <h2>Little Herb Places is a unique platform dedicated to supporting neighborhood herbal shops and fostering a thriving community of herbal enthusiasts.</h2>
-      <p>By focusing on small, independent stores, the platform shines a spotlight on the heart and soul of botanical medicine—local, community-driven businesses that often serve as the backbone of herbal traditions. Little Herb Places celebrates these spaces not just as retail outlets but as hubs of knowledge, connection, and healing.</p>
-      <p>The initiative was born out of a project for the Wildflower School of Botanical Medicine in Austin, Texas: a program that instills a deep appreciation for the natural world and the role plants play in health and wellness. Initially conceived as a way to document and showcase herbal shops, Little Herb Places continues to evolve and intends to serve comprehensive resource for anyone interested in herbal medicine. Whether you're a seasoned herbalist, a curious beginner, or someone seeking alternative health solutions, this platform aims to connect you with the wisdom and resources found in neighborhood herbal shops.</p>
-      <p>Through careful curation, Little Herb Places highlights the diversity and richness of herbal shops from around the globe. Each shop featured on the platform has its own unique story, reflecting the local culture, traditions, and practices that shape its approach to herbal medicine. By bringing these stories to a wider audience, the platform not only promotes the shops but also helps preserve and share the invaluable knowledge and heritage they represent.</p>
-      <p>In a world where large-scale commercial interests often overshadow smaller enterprises, Little Herb Places stands as a testament to the importance of community and authenticity. It provides a space where herbal shops can gain visibility, connect with like-minded individuals, and continue to thrive in a supportive environment. At its core, Little Herb Places is more than just a directory; it's a celebration of the enduring relationship between people and plants.</p>
-    </div>
+      <Helmet>
+        <title>Little Herb Places | About</title>
+      </Helmet>
+      <div className={styles.container}>
+        <p>Welcome to Little Herb Places!</p>
+        <p>We invite you to embark on a journey through the vibrant world of herbalism. Our site is your resource for discovering herb shops around the world and local herbalists and medicine makers around the corner.</p>
+        <p>Our mission is to bridge the gap between herbalists and their communities. To give a voice to those who craft medicine from Earth&apos;s finest plants and herbs while providing a place for them to share their stories and sell their products.</p>
+        <p>Join us in celebrating the art of herbalism and connect with your local community.</p>
+      </div>
     </>
   )
 }
