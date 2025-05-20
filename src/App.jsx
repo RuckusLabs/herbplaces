3ad6e67ba7e802819b9a2c846bf03a91f0c55d86
Damian Makki <damian@makki.pro>
2025-05-20 15:01:47 -0700

    adjust routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,56 @@
 import Footer from "./components/Footer/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Nav from './components/Nav/Nav';
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Shop from "./pages/Shop/Shop";
 import Map from "./pages/Map/Map";
 import Place from "./pages/Place/Place";
+import TheLittleGarden from "./pages/TheLittleGarden/TheLittleGarden";
 
-function App() {
+const defaultConfig = {
+  navVariant: 'garden'
+};
+
+const routes = [
+  { path: '/', element: <Home />, navVariant: 'bleed' },
+  { path: '/map', element: <Map /> },
+  { path: '/shop', element: <Shop /> },
+  { path: '/about', element: <About /> },
+  { path: '/the-little-garden', element: <TheLittleGarden />, navVariant: 'bleed' },
+  { path: '/place/:slug', element: <Place /> }
+].map(route => ({ ...defaultConfig, ...route }));
+
+function AppContent() {
+  const location = useLocation();
+  const currentRoute = routes.find(route => {
+    // Handle dynamic routes like /place/:slug
+    if (route.path.includes(':')) {
+      return location.pathname.startsWith(route.path.split(':')[0]);
+    }
+    return route.path === location.pathname;
+  });
+  const navVariant = currentRoute?.navVariant || defaultConfig.navVariant;
 
   return (
-    <Router>
-      <Nav />
+    <>
+      <Nav variant={navVariant} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/place/:slug" element={<Place />} />
+        {routes.map(route => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
       </Routes>
       <Footer />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
     </Router>
-  )
+  );
 }
 
 export default App
